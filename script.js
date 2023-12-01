b14345ae9a13a8760ee271811a4e8ea4e53946c2
Anushka Singh <103564913+anushka077@users.noreply.github.com>
2023-12-01 23:45:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+<script>
+    var currentProductIndex = 0;
+    var productCards = document.getElementsByClassName("product-card");
 
+    function showProduct(index) {
+      for (var i = 0; i < productCards.length; i++) {
+        productCards[i].classList.remove("active");
+      }
+      productCards[index].classList.add("active");
+    }
+
+    function rotateLeft() {
+      currentProductIndex = (currentProductIndex - 1 + productCards.length) % productCards.length;
+      showProduct(currentProductIndex);
+    }
+
+    function rotateRight() {
+      currentProductIndex = (currentProductIndex + 1) % productCards.length;
+      showProduct(currentProductIndex);
+    }
+
+    function autoRotate() {
+      rotateRight();
+    }
+
+    // Set up the automatic rotation
+    var intervalId = setInterval(autoRotate, 3000);
+
+    // Stop automatic rotation when hovering over the carousel
+    document.querySelector('.carousel-container').addEventListener('mouseenter', function () {
+      clearInterval(intervalId);
+    });
+
+    // Resume automatic rotation when not hovering
+    document.querySelector('.carousel-container').addEventListener('mouseleave', function () {
+      intervalId = setInterval(autoRotate, 3000);
+    });
+  </script>
